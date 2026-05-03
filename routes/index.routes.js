31b19875b58a25a2c28b1f2d92ae762b081713cb
Valeriam28 <valeriamundaray@gmail.com>
2026-05-03 15:34:23 -0400

    eliminacion extras (carpeta teams )

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,10 +1,8 @@
 import { Router } from 'express';
 
-import teamsRouter from '../modules/teams/teams.routes.js';
 import boardgamesRouter from '../modules/boardgames/boardgames.routes.js';
 const indexRouter = Router();
 
-indexRouter.use("/teams", teamsRouter);
 indexRouter.use("/boardgames", boardgamesRouter);
 
 export default indexRouter;
