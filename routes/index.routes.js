444bea5ae8b18978a131fd391bd676847c851a9e
Valeriam28 <valeriamundaray@gmail.com>
2026-05-03 13:18:24 -0400

    Primer commit

---
+++ routes/index.routes.js
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+
+import teamsRouter from '../modules/teams/teams.routes.js';
+
+const indexRouter = Router();
+
+indexRouter.use("/teams", teamsRouter);
+
+export default indexRouter;
